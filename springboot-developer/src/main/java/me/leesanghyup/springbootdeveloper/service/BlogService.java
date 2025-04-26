261c3db17c9dd896b294f6bdf8f74c7aa593a6fb
SanghyupTwo <dwc07238@naver.com>
2025-04-26 17:03:38 +0900

    feat: 게시글 수정 API 구현 및 테스트 추가 (PUT /api/articles/{id})

--- springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/service/BlogService.java
+++ springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/service/BlogService.java
@@ -1,11 +1,14 @@
 package me.leesanghyup.springbootdeveloper.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.leesanghyup.springbootdeveloper.domain.Article;
 import me.leesanghyup.springbootdeveloper.dto.AddArticleRequest;
+import me.leesanghyup.springbootdeveloper.dto.UpdateArticleRequest;
 import me.leesanghyup.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.beans.Transient;
 import java.util.List;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
@@ -33,5 +36,15 @@ public class BlogService {
     // 데이터베이스에서 데이터를 삭제
     public void delete(Long id) {
         blogRepository.deleteById(id);
+    }
+
+    @Transactional // 트랜잭션 메서드
+    public Article update(Long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
--- springboot-developer/src/test/java/me/leesanghyup/springbootdeveloper/controller/BlogApiControllerTest.java
+++ springboot-developer/src/test/java/me/leesanghyup/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -3,6 +3,7 @@ package me.leesanghyup.springbootdeveloper.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.leesanghyup.springbootdeveloper.domain.Article;
 import me.leesanghyup.springbootdeveloper.dto.AddArticleRequest;
+import me.leesanghyup.springbootdeveloper.dto.UpdateArticleRequest;
 import me.leesanghyup.springbootdeveloper.repository.BlogRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -142,5 +143,40 @@ class BlogApiControllerTest {
 
         // then
         List<Article> articles = blogRepository.findAll();
+    }
+
+    @DisplayName("updateArticle: 블로그 글 수정에 성공한다.")
+    @Test
+    public void updateArticle() throws Exception {
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        final String newTitle = "new title";
+        final String newContent = "new content";
+
+        UpdateArticleRequest request = new UpdateArticleRequest(newTitle, newContent);
+
+        // when
+        ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        // then
+        result.andExpect(status().isOk());
+
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+
+        assertThat(article.getTitle()).isEqualTo(newTitle);
+        assertThat(article.getContent()).isEqualTo(newContent);
+
+        // when
+
     }
 }
