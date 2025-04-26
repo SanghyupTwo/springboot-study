2e6498d9e748d3d142c736e20fd3a77d089bf702
SanghyupTwo <dwc07238@naver.com>
2025-04-26 18:17:41 +0900

    feat: Article 생성/수정일 필드 추가 및 글 목록 화면에 날짜 표시 기능 추가

--- springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/controller/BlogViewController.java
+++ springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.leesanghyup.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.leesanghyup.springbootdeveloper.domain.Article;
 import me.leesanghyup.springbootdeveloper.dto.ArticleListViewResponse;
+import me.leesanghyup.springbootdeveloper.dto.ArticleViewResponse;
 import me.leesanghyup.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); // 블로그 글 리스트 저장
 
         return "articleList"; // articleList.html라는 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable("id") Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
