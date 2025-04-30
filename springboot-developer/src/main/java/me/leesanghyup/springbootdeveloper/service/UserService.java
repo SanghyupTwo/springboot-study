70d879a3f9804a49e70ab09a634043779501a935
SanghyupTwo <dwc07238@naver.com>
2025-04-30 14:21:56 +0900

    feat: 회원가입 기능 구현 (DTO, Service, Controller 추가)

---
+++ springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/service/UserService.java
@@ -0,0 +1,26 @@
+package me.leesanghyup.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.leesanghyup.springbootdeveloper.domain.User;
+import me.leesanghyup.springbootdeveloper.dto.AddUserRequest;
+import me.leesanghyup.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.PrivateKey;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
