c790825680400104561eec2da9bf727c0b662698
SanghyupTwo <dwc07238@naver.com>
2025-05-01 12:04:46 +0900

    feat: OAuth 비즈니스 로직 구성

--- springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/service/BlogService.java
+++ springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/service/BlogService.java
@@ -8,7 +8,6 @@ import me.leesanghyup.springbootdeveloper.dto.UpdateArticleRequest;
 import me.leesanghyup.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
-import java.beans.Transient;
 import java.util.List;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
@@ -18,8 +17,8 @@ public class BlogService {
     private final BlogRepository blogRepository;
 
     // 블로그 글 추가 메서드
-    public Article save(AddArticleRequest request) {
-        return blogRepository.save(request.toEntity());
+    public Article save(AddArticleRequest request, String userName) {
+        return blogRepository.save(request.toEntity(userName));
     }
 
     // 데이터베이스에 저장되어 있는 모든 글 가져오기
--- springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/service/UserService.java
+++ springboot-developer/src/main/java/me/leesanghyup/springbootdeveloper/service/UserService.java
@@ -15,18 +15,24 @@ import java.security.PrivateKey;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Long save(AddUserRequest dto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
                 // 패스워드 암호화
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(encoder.encode(dto.getPassword()))
                 .build()).getId();
     }
 
     public User findById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalReceiveException("Unexpected user"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 }
